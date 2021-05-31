0f111319fe9ce2b3d7207ed25dc5907b975f2641
ysouaardi <yosua.ardi@ti.ukdw.ac.id>
2021-05-31 22:51:22 +0700

    - testing punchOut

--- src/test/java/PunchInOut.java
+++ src/test/java/PunchInOut.java
@@ -28,13 +28,21 @@ public class PunchInOut {
         Login(driver);
 
         //panggil test case
-        Test1(driver);
-        Test2(driver);
-        Test3(driver);
-        Test4(driver);
-        Test5(driver);
-        Test6(driver);
+//        Test1(driver);
+//        Test2(driver);
+//        Test3(driver);
+//        Test4(driver);
+//        Test5(driver);
+//        Test6(driver);
 
+        //Test Punch Out
+        PunchIn(driver);
+//        Test1Out(driver);
+//        Test2Out(driver);
+//        Test3Out(driver);
+//        Test4Out(driver);
+//        Test5Out(driver);
+        Test6Out(driver);
     }
 
 //    public static void testPunchInOut(WebDriver driver){
@@ -209,6 +217,208 @@ public class PunchInOut {
 
     //input note lebih dari 250 karakter
     public static void Test6(WebDriver driver){
+        SoftAssert softAssert = new SoftAssert();
+        WebElement note = driver.findElement(By.xpath("//*[@id = 'attendance_note']"));
+        WebElement btnPunch = driver.findElement(By.xpath("//*[@id = 'btnPunch']"));
+
+        //data testing
+        String urlTujuan = "https://opensource-demo.orangehrmlive.com/index.php/attendance/punchOut";
+        CharSequence test = "";
+        for (int i=0; i<260; i++){
+            test= test +"a";
+        }
+
+        //testing
+        System.out.println("testing test case input note lebih dari 250 karakter");
+        note.clear();
+        note.sendKeys(test);
+
+        btnPunch.click();
+        softAssert.assertEquals(driver.getCurrentUrl(), urlTujuan, "test input lebih dari 250 karakter gagal");
+
+        softAssert.assertAll();
+        driver.quit();
+    }
+
+    public static void PunchIn(WebDriver driver){
+        SoftAssert softAssert = new SoftAssert();
+        WebElement date = driver.findElement(By.xpath("//*[@id = 'attendance_date']"));
+        WebElement time = driver.findElement(By.xpath("//*[@id = 'attendance_time']"));
+        WebElement note = driver.findElement(By.xpath("//*[@id = 'attendance_note']"));
+        WebElement btnPunch = driver.findElement(By.xpath("//*[@id = 'btnPunch']"));
+        String tanggal = "2021-06-02";
+        String jam = "12:00";
+        date.clear();
+        date.sendKeys(tanggal);
+        time.clear();
+        time.sendKeys(jam);
+        note.sendKeys("");
+        btnPunch.click();
+    }
+
+    //Jika berhasil
+    public static void Test1Out(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.urlToBe("https://opensource-demo.orangehrmlive.com/index.php/attendance/punchOut"));
+        SoftAssert softAssert = new SoftAssert();
+        WebElement date = driver.findElement(By.xpath("//*[@id = 'attendance_date']"));
+        WebElement time = driver.findElement(By.xpath("//*[@id = 'attendance_time']"));
+        WebElement note = driver.findElement(By.xpath("//*[@id = 'attendance_note']"));
+        WebElement btnPunch = driver.findElement(By.xpath("//*[@id = 'btnPunch']"));
+
+        String tanggal = "2021-06-02";
+        String jam = "12:01";
+        date.clear();
+        date.sendKeys(tanggal);
+        time.clear();
+        time.sendKeys(jam);
+        note.sendKeys("test case punch out 1");
+
+        btnPunch.click();
+        wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.urlToBe("https://opensource-demo.orangehrmlive.com/index.php/attendance/punchIn"));
+        softAssert.assertEquals(btnPunch.getAttribute("value"), "In");
+        softAssert.assertAll();
+        btnPunch.click();
+        driver.quit();
+    }
+
+    //Test waktu kurang dari Punch In
+    public static void Test2Out(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.urlToBe("https://opensource-demo.orangehrmlive.com/index.php/attendance/punchOut"));
+        SoftAssert softAssert = new SoftAssert();
+        WebElement date = driver.findElement(By.xpath("//*[@id = 'attendance_date']"));
+        WebElement time = driver.findElement(By.xpath("//*[@id = 'attendance_time']"));
+        WebElement note = driver.findElement(By.xpath("//*[@id = 'attendance_note']"));
+        WebElement btnPunch = driver.findElement(By.xpath("//*[@id = 'btnPunch']"));
+        String urlTujuan = "https://opensource-demo.orangehrmlive.com/index.php/attendance/punchIn";
+        String tanggal = "2021-06-02";
+        String jam = "11:59";
+        date.clear();
+        date.sendKeys(tanggal);
+        time.clear();
+        time.sendKeys(jam);
+        note.sendKeys("test case punch out 2");
+
+        btnPunch.click();
+        System.out.println(driver.getCurrentUrl());
+        softAssert.assertEquals(driver.getCurrentUrl(), urlTujuan, "test case punch out 2 gagal");
+        softAssert.assertAll();
+        btnPunch.click();
+        driver.quit();
+    }
+
+    //Test tanggal kurang dari Punch In
+    public static void Test3Out(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.urlToBe("https://opensource-demo.orangehrmlive.com/index.php/attendance/punchOut"));
+        SoftAssert softAssert = new SoftAssert();
+        WebElement date = driver.findElement(By.xpath("//*[@id = 'attendance_date']"));
+        WebElement time = driver.findElement(By.xpath("//*[@id = 'attendance_time']"));
+        WebElement note = driver.findElement(By.xpath("//*[@id = 'attendance_note']"));
+        WebElement btnPunch = driver.findElement(By.xpath("//*[@id = 'btnPunch']"));
+        String urlTujuan = "https://opensource-demo.orangehrmlive.com/index.php/attendance/punchIn";
+        String tanggal = "2021-06-01";
+        String jam = "12:00";
+        date.clear();
+        date.sendKeys(tanggal);
+        time.clear();
+        time.sendKeys(jam);
+        note.sendKeys("test case punch out 2");
+
+        btnPunch.click();
+        System.out.println(driver.getCurrentUrl());
+        softAssert.assertEquals(driver.getCurrentUrl(), urlTujuan, "test case punch out 3 gagal");
+        softAssert.assertAll();
+        btnPunch.click();
+        driver.quit();
+    }
+
+    //input date tidak sesuai format
+    public static void Test4Out(WebDriver driver){
+        SoftAssert softAssert = new SoftAssert();
+        WebElement date = driver.findElement(By.xpath("//*[@id = 'attendance_date']"));
+        WebElement btnPunch = driver.findElement(By.xpath("//*[@id = 'btnPunch']"));
+
+        //data testing
+        String urlTujuan = "https://opensource-demo.orangehrmlive.com/index.php/attendance/punchOut";
+        CharSequence[] test = new String[12];
+        test[0] = "";                  //coba String kosong
+        test[1] = "a";                 //coba isi char
+        test[2] = "aaaa-aa-aa";        //coba isi alfabet
+        test[3] = "1212-aa-12";        //coba isi alpanumerik
+        test[4] = "@@@@-@@-##";        //coba isi simbol
+        test[5] = "22221212";          //coba angka tanpa pemisah
+        test[6] = "9999-10-10";        //coba tahun 9999
+        test[7] = "0000-10-10";        //coba tahun 0000
+        test[8] = "2021-13-10";        //coba bulan 13
+        test[9] = "2021-00-10";        //coba bulan 00
+        test[10] = "2021-10-32";        //coba tanggal 32
+        test[11] = "2021-10-00";        //coba tanggal 00
+
+        //coba input date
+        for (int i=0; i<test.length; i++){
+            date.clear();
+            CharSequence temp = test[i];
+            System.out.println("testing test case ke:"+(i+1)+", isi test case : "+temp);
+            date.sendKeys(temp);
+            btnPunch.click();
+            System.out.println(driver.getCurrentUrl());
+            softAssert.assertEquals(driver.getCurrentUrl(), urlTujuan, "test ke-"+(i+1)+" gagal");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        softAssert.assertAll();
+        btnPunch.click();
+        driver.quit();
+    }
+
+    //input time tidak sesuai format
+    public static void Test5Out(WebDriver driver){
+        SoftAssert softAssert = new SoftAssert();
+        WebElement time = driver.findElement(By.xpath("//*[@id = 'attendance_time']"));
+        WebElement btnPunch = driver.findElement(By.xpath("//*[@id = 'btnPunch']"));
+
+        //data testing
+        String urlTujuan = "https://opensource-demo.orangehrmlive.com/index.php/attendance/punchOut";
+        CharSequence[] test = new String[10];
+        test[0] = "";                  //coba String kosong
+        test[1] = "a";                 //coba isi char
+        test[2] = "aa:bb";        //coba isi alfabet
+        test[3] = "a1:b2";        //coba isi alpanumerik
+        test[4] = "@@:$$";        //coba isi simbol
+        test[5] = "1212";          //coba angka tanpa pemisah
+        test[6] = "25:00";        //coba jam 25
+        test[7] = "-10:00";        //coba jam minus
+        test[8] = "12:61";        //coba menit 61
+        test[9] = "12:-10";        //coba menit minus
+
+        //coba input time
+        for (int i=0; i<test.length; i++){
+            time.clear();
+            CharSequence temp = test[i];
+            System.out.println("testing test case ke:"+(i+1)+", isi test case : "+temp);
+            time.sendKeys(temp);
+            btnPunch.click();
+            System.out.println(driver.getCurrentUrl());
+            softAssert.assertEquals(driver.getCurrentUrl(), urlTujuan, "test ke-"+(i+1)+" gagal");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        softAssert.assertAll();
+        btnPunch.click();
+        driver.quit();
+    }
+
+    //input note lebih dari 250 karakter
+    public static void Test6Out(WebDriver driver){
         SoftAssert softAssert = new SoftAssert();
         WebElement note = driver.findElement(By.xpath("//*[@id = 'attendance_note']"));
         WebElement btnPunch = driver.findElement(By.xpath("//*[@id = 'btnPunch']"));
